4cd480ed4583a39098853d7dff5321068970eeb4
shimada337 <eisukeqoo@icloud.com>
2022-03-07 01:59:24 +0000

    [add]indexページリンク作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,11 +6,21 @@ class BooksController < ApplicationController
   end
   
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/show'
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
