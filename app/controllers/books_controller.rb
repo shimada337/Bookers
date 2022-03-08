3c90bccd3e56660b97da67f0a6c11edd1374894d
shimada337 <eisukeqoo@icloud.com>
2022-03-08 07:48:53 +0000

    [finish]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,9 +23,12 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id), flash: {update: "Book was successfully updated."}
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id), flash: {update: "Book was successfully updated."}
+    else
+    render :edit
+    end
   end
   
   def destroy
